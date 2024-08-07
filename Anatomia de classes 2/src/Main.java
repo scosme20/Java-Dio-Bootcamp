1083235c92994ba8021d2bc9f32c5b5c8c2aa2c9
scosme20 <scosmeagostinho@hotmail.com>
2024-08-06 22:04:15 -0300

    Aula anatomia de classe 1

--- Anatomia de classes 2/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        final String nome = "Sebastião Cosme";
-                int idade = 22;
-
-        System.out.println("Bom dia, meu nome é " + nome + " e eu tenho " + idade + " anos");
-    }
-}
