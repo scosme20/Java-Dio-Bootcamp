819b2ae76891c27b6d36e53546320281591f1927
scosme20 <scosmeagostinho@hotmail.com>
2024-08-06 21:05:36 -0300

    Removendo o diretório Anatomia de classes 1 do índice

---
+++ Anatomia de classes 2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        final String nome = "Sebastião Cosme";
+                int idade = 22;
+
+        System.out.println("Bom dia, meu nome é " + nome + " e eu tenho " + idade + " anos");
+    }
+}
